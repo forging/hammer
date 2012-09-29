ffdcb8861daec2ba143018bd18797ce6c3c794c0
unknown <Administrator@PC-201105102037.(none)>
2012-09-30 00:16:31 +0800

    initial commit

---
+++ wp-config.php
@@ -0,0 +1,30 @@
+<?php
+$services = getenv("VCAP_SERVICES");
+$services_json = json_decode($services,true);
+$mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+
+// ** MySQL settings from resource descriptor ** //
+define('DB_NAME', $mysql_config["db_b2d26c76"]);
+define('DB_USER', $mysql_config["user_b2d26c76"]);
+define('DB_PASSWORD', $mysql_config["GtCOFVwR;)h18V"]);
+define('DB_HOST', $mysql_config["a.db.shared.orchestra.io"]);
+define('DB_PORT', $mysql_config["port"]);
+
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+define ('WPLANG', '');
+define('WP_DEBUG', false);
+
+require('wp-salt.php');
+
+$table_prefix  = 'wp_';
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
